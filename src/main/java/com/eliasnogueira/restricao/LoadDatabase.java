527954f5ac1d5e6a4f6565ee4dde1309b85238bc
eliasnogueira <elias@backbase.com>
2020-02-01 22:31:35 +0100

    refatoracao do carregamento de dados

--- src/main/java/com/eliasnogueira/restricao/LoadDatabase.java
+++ src/main/java/com/eliasnogueira/restricao/LoadDatabase.java
@@ -27,6 +27,7 @@ package com.eliasnogueira.restricao;
 import com.eliasnogueira.restricao.entity.Restricao;
 import com.eliasnogueira.restricao.entity.TipoRestricao;
 import com.eliasnogueira.restricao.repository.RestricaoRepository;
+import java.util.HashMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,17 +38,25 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabaseRestricao(RestricaoRepository restricaoRepository) {
         return args -> {
-            restricaoRepository.save(new Restricao("97093236014", TipoRestricao.BLOQUEIO_JURICIAL.value()));
-            restricaoRepository.save(new Restricao("60094146012", TipoRestricao.CARTAO_CREDITO.value()));
-            restricaoRepository.save(new Restricao("84809766080", TipoRestricao.BANCARIA.value()));
-            restricaoRepository.save(new Restricao("62648716050", TipoRestricao.SPC.value()));
-            restricaoRepository.save(new Restricao("26276298085", TipoRestricao.SPC.value()));
-            restricaoRepository.save(new Restricao("01317496094", TipoRestricao.CARTAO_CREDITO.value()));
-            restricaoRepository.save(new Restricao("55856777050", TipoRestricao.BANCARIA.value()));
-            restricaoRepository.save(new Restricao("19626829001", TipoRestricao.BLOQUEIO_JURICIAL.value()));
-            restricaoRepository.save(new Restricao("24094592008", TipoRestricao.BANCARIA.value()));
-            restricaoRepository.save(new Restricao("58063164083", TipoRestricao.BANCARIA.value()));
+            dataToInsert().forEach((cpf, restricao) -> restricaoRepository.save(new Restricao(cpf, restricao)));
         };
+    }
+
+    private HashMap<String, String> dataToInsert() {
+        HashMap<String, String> data = new HashMap<>();
+
+        data.put("97093236014", TipoRestricao.BLOQUEIO_JURICIAL.value());
+        data.put("60094146012", TipoRestricao.CARTAO_CREDITO.value());
+        data.put("84809766080", TipoRestricao.BANCARIA.value());
+        data.put("62648716050", TipoRestricao.SPC.value());
+        data.put("26276298085", TipoRestricao.SPC.value());
+        data.put("01317496094", TipoRestricao.CARTAO_CREDITO.value());
+        data.put("55856777050", TipoRestricao.BANCARIA.value());
+        data.put("19626829001", TipoRestricao.BLOQUEIO_JURICIAL.value());
+        data.put("24094592008", TipoRestricao.BANCARIA.value());
+        data.put("58063164083", TipoRestricao.BANCARIA.value());
+
+        return data;
     }
 
 }
